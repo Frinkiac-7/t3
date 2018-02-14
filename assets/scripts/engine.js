3f0c271d37b028db95f68031d3e0389a954f8562
frinkiac-7.net <rob@frinkiac-7.net>
2018-02-13 21:54:19 -0500

    Fix grunt serve image fix (3rd attempt)   * Initial add of the remote image url was incorrect:     - Was missing the .jpg extension     - Was missing the https://i. prefix in the URL

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -238,13 +238,13 @@ const recordMove = function (player, gridPos) {
     gameArray[gameObject[gridPos].arrayIndex] = player
     // Update the DOM to indicate the space has been selected
     // $('#' + gridPos).text(player + ' takes the square')
-    $('#' + gridPos).attr('src', 'https://imgur.com/PYVoMNB')
+    $('#' + gridPos).attr('src', 'https://i.imgur.com/PYVoMNB.jpg')
   } else { // Will only execute if grid position is anything but "!"
     // Reduce moveCount to allow current player to try again
     --moveCount
     // Update the DOM to let current player know who already owns that square
     // $('#' + gridPos).text(gameObject[gridPos].player + ' ALREADY TOOK THIS SQUARE')
-    $('#' + gridPos).attr('src', 'https://imgur.com/0eXPHuG')
+    $('#' + gridPos).attr('src', 'https://i.imgur.com/0eXPHuG.jpg')
   }
 }
 
